6c50889038b7960d23db74a1d5861292bdd3b16a
Adam Oken <Adam_Oken@Adams-MacBook-Air.local>
2016-02-29 22:23:15 -0700

    finsihed views

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,16 +1,16 @@
 /* GET homepage */
 
 module.exports.homeList = function(req, res){
-	res.render('index', {title: 'Home'})
+	res.render('locations-list', {title: 'Home'})
 }
 
 /* GET locations homepage */
 module.exports.locationInfo = function(req, res){
-	res.render('index', {title: 'Location info'})
+	res.render('location-info', {title: 'Location info'})
 }
 
 /*GET Revies Page */
 
 module.exports.addReview = function(req, res){
-	res.render('index', {title: 'Add Review'})
+	res.render('location-review-form', {title: 'Add Review'})
 }
